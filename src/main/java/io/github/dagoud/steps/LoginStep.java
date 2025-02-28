d2eb27edb21bb867b6845b783c38206e7d04ece4
David Goudie <dagoudie@Davids-Mac-mini.local>
2025-02-28 10:00:58 +0000

    DG add login step

--- src/main/java/io/github/dagoud/steps/LoginStep.java
+++ src/main/java/io/github/dagoud/steps/LoginStep.java
@@ -6,13 +6,18 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginStep {
 
+    private static final String SAUCE_DEMO_URL = "https://www.saucedemo.com";
     private WebDriver driver;
 
     @Before
@@ -31,16 +36,29 @@ public class LoginStep {
     @Given("User is on Login Page")
     public void userIsOnLoginPage() {
         System.out.println("Test 1");
+        driver.get(SAUCE_DEMO_URL);
+        Duration duration = Duration.ofSeconds(10);
+        Wait<WebDriver> wait = new WebDriverWait(driver, duration)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("login_logo")));
     }
 
     @When("User fill username and password")
     public void userFillUsernameAndPassword() {
         System.out.println("Test 2");
+
     }
 
     @And("User click Login button")
     public void userClickLoginButton() {
         System.out.println("Test 3");
+        Duration duration = Duration.ofSeconds(10);
+        WebElement loginButton = new WebDriverWait(driver, duration)
+                .until(ExpectedConditions.elementToBeClickable(By.id("login-button")));
+        loginButton.click();
     }
 
     @Then("User will redirect to Products page")
