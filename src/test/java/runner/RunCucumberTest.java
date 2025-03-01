9b115a325be5bef5676cd73b7707aa3478058e0b
David Goudie <dagoudie@Davids-Mac-mini.local>
2025-03-01 12:20:43 +0000

    DG remove warning msg by adding glue property to runner

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -2,12 +2,14 @@ package runner;
 
 import org.junit.platform.suite.api.*;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameters(value = {
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.dagoud"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber/report.html")
 })
